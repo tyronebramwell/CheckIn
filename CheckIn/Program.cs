3c98c1431dadd0280d202f2d666470090e3c108b
Tyrone Bramwell <Tyrone.Bramwell@And.Digital>
2026-05-28 22:11:42 +0100

    chore: add aggressive cache busting to Nginx config
    - Added Cache-Control headers to nginx.conf to prevent mobile browsers from aggressively caching outdated Blazor WASM DLLs.

--- CheckIn/Program.cs
+++ CheckIn/Program.cs
@@ -15,7 +15,7 @@ CultureInfo.DefaultThreadCurrentUICulture = culture;
 builder.Services.AddScoped(sp =>
     new HttpClient
     {
-        BaseAddress = new Uri("https://localhost:5001")
+        BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)
     }
 );
 builder.Services.AddScoped<CheckIn.Services.AuthService>();
